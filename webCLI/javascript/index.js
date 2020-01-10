d22b09ff170b93d131094bb941ac6fa6213e5d2b
Chance Shaffer <shafferchance@gmail.com>
2020-01-09 19:50:10 -0500

    Playing with code some progress made but not much. Clear will still only delete half of the elements at the most.

--- webCLI/javascript/index.js
+++ webCLI/javascript/index.js
@@ -1,34 +1,38 @@
-document.addEventListener("load", () => {
+window.addEventListener("load", () => {
         const consoleInputContainer = document.querySelector(".console-input-area");
         const consoleInput = document.querySelector(".console-input");
         const consoleCursor = document.querySelector(".console-cursor");
         const consoleMain = document.querySelector(".console-main");
 
         consoleInput.addEventListener("keypress", e => {
-        if (e.key === "Enter") {
-            const cmd = document.createElement("div");
-            let idx = consoleMain.children.length - 2;
-            cmd.classList.add(`cmd-${idx}`);
-            cmd.innerHTML = consoleInput.value;
-            consoleInput.value = "";
-            consoleMain.insertBefore(cmd, consoleInputContainer);
-        }
+            if (e.key === "Enter") {
+                const cmd = document.createElement("div");
+                let idx = consoleMain.children.length - 2;
+                let val = consoleInput.value;
+                cmd.classList.add(`cmd-${idx}`);
+                cmd.innerHTML = consoleInput.value;
+                consoleInput.value = "";
+                consoleMain.insertBefore(cmd, consoleInputContainer);
+                handleCommand(val);
+            }
         });
 
         consoleInput.addEventListener("blur", e => {
-        consoleCursor.classList.remove("blink");
+            consoleCursor.classList.remove("blink");
         });
 
         consoleInputContainer.addEventListener("click", e => {
-        consoleInputContainer.children[1].focus();
-        consoleCursor.classList.add("blink");
+            consoleInputContainer.children[1].focus();
+            consoleCursor.classList.add("blink");
         });
 
 
     const handleCommand = input => {
-        switch(input) {
-        case input === "clear":
-            
+        console.log(consoleMain.children);
+        if (input === "clear") {
+            Array(consoleMain.children).forEach(ele => {
+                console.log(ele);
+            });
         }
     }
 });
